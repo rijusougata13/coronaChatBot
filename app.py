66a7b998b2e00d528413295dba1b0d4684b80e91
rijusougata13 <rijusougata13.com>
2020-04-10 17:08:49 +0530

    added chatbot

---
+++ app.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 15:45:59 2020
+
+@author: sougata
+"""
+
+#imports
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load('model.tflearn')
+except:
+    tensorflow.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+    
+
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/get")
+#function for the bot response
+def get_bot_response():
+    def bag_of_words(s, words):
+        bag = [0 for _ in range(len(words))]
+    
+        s_words = nltk.word_tokenize(s)
+        s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+        for se in s_words:
+            for i, w in enumerate(words):
+                if w == se:
+                    bag[i] = 1
+                
+        return numpy.array(bag)
+
+
+
+
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = request.args.get('msg')
+        if inp.lower() == "quit":
+            break
+    
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+    
+        if results[results_index]>0.7:
+                
+            for tg in data['intents']:
+                if(tg['tag']==tag):
+                    responses=tg['responses']
+            print('BOT: ',end='')
+            return(random.choice(responses))
+        else:
+            
+            return('BOT : i didnot get it :< ask me another question')
+    return ('hi')
+    
+    
+    
+#    userText = request.args.get('msg')
+#    return str(englishBot.get_response(userText))
+if __name__ == "__main__":
+    app.run()
